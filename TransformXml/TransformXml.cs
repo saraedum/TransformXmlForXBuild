e0a6d4de5daa3c60fa8ab64c9176d3d6a41aa2f1
Julian Rüth <julian.rueth@fsfe.org>
2016-06-02 17:40:16 +0200

    Initial commit.

---
+++ TransformXml/TransformXml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Build.Utilities;
+using Microsoft.Build.Framework;
+using Microsoft.Web.XmlTransform;
+
+namespace MSBuildTasks {
+	public class TransformXml : Task {
+		[Required]
+		public ITaskItem Source { get; set; }
+
+		[Required]
+		public ITaskItem Transform { get; set; }
+
+		[Required]
+		public ITaskItem Destination { get; set; }
+
+		public override bool Execute() {
+			var originalFileXml = new System.Xml.XmlDocument();
+			originalFileXml.Load(Source.ItemSpec);
+
+			using (var xmlTransform = new XmlTransformation(Transform.ItemSpec)) {
+				if (xmlTransform.Apply(originalFileXml) == false)
+					return false;
+
+				// originalFileXml is now transformed
+				originalFileXml.Save(Destination.ItemSpec);
+			}
+
+			return true;
+		}
+	}
+}
